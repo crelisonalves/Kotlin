90d66c2399193bb4764286d9bdc219c46876de1f
Crelison Alves <70458513+crelisonalves@users.noreply.github.com>
2021-07-13 21:58:32 -0300

    Create 01 - introdução.kt

---
+++ 01 - introdução.kt
@@ -0,0 +1,13 @@
+/**
+ * Introdução ao Kotlin
+ * play.kotlinlang.org
+ * Autor: Crelison Alves
+ * Data: 13/07/2021 
+ *
+ * Este programa exibe um texto no terminal.
+ *
+ */
+
+fun main() {
+    println("Olá mundo!!!")
+}
